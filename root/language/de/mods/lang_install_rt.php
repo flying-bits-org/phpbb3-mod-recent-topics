d9cb00133b743d50692e2e024406296bdc1a3865
Joas Schilling <nickvergessen@gmx.de>
2008-07-17 18:06:08 +0000

    MODx 1.2.0

---
+++ root/language/de/mods/lang_install_rt.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+*
+* @package - NV recent topics
+* @version $Id: lang_install_rt.php 70 2008-01-06 11:10:11Z nickvergessen $
+* @copyright (c) nickvergessen ( http://mods.flying-bits.org/ )
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'INSTALLER_INTRO'					=> 'Intro',
+	'INSTALLER_INTRO_WELCOME'			=> 'Willkommen zur MOD-Installation',
+	'INSTALLER_INTRO_WELCOME_NOTE'		=> 'Bitte wähle aus, was du tun möchtest.',
+
+	'INSTALLER_INSTALL'					=> 'Installieren',
+	'INSTALLER_INSTALL_MENU'			=> 'Installation',
+	'INSTALLER_INSTALL_SUCCESSFUL'		=> 'Installation der MOD v%s war erfolgreich.',
+	'INSTALLER_INSTALL_UNSUCCESSFUL'	=> 'Installation der MOD v%s war <strong>nicht</strong> erfolgreich.',
+	'INSTALLER_INSTALL_VERSION'			=> 'Installiere MOD v%s',
+	'INSTALLER_INSTALL_WELCOME'			=> 'Willkommen zur Installation',
+	'INSTALLER_INSTALL_WELCOME_NOTE'	=> 'Wenn du den MOD installierst, werden möglicherweise vorhandene Datenbanktabellen mit gleichem Namen gelöscht.',
+
+	'INSTALLER_NEEDS_FOUNDER'			=> 'Du musst als Gründer eingeloggt sein.',
+
+	'INSTALLER_UPDATE'					=> 'Update',
+	'INSTALLER_UPDATE_MENU'				=> 'Updatemenü',
+	'INSTALLER_UPDATE_NOTE'				=> 'Update MOD von v%s nach v%s',
+	'INSTALLER_UPDATE_SUCCESSFUL'		=> 'Update der MOD von v%s nach v%s war erfolgreich.',
+	'INSTALLER_UPDATE_UNSUCCESSFUL'		=> 'Update der MOD von v%s nach v%s war <strong>nicht</strong> erfolgreich.',
+	'INSTALLER_UPDATE_VERSION'			=> 'Update MOD von v',
+	'INSTALLER_UPDATE_WELCOME'			=> 'Willkommen zum Update',
+
+	'RT_UNSUPPORTED'					=> 'Entschuldigung, deine Datenbank wird nicht unterstützt.',
+
+	'WARNING'							=> 'Warnung',
+));
+
+?>
