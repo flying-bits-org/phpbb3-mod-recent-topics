7d50faec80bea120c907ef8e2885db3b9f6753c8
Joas Schilling <nickvergessen@gmx.de>
2009-04-23 10:55:07 +0000

    Argl!

--- root/language/en/mods/info_acp_recenttopics.php
+++ root/language/en/mods/info_acp_recenttopics.php
@@ -4,7 +4,7 @@
 *
 * @package - NV recent topics
 * @version $Id$
-* @copyright (c) nickvergessen ( http://mods.flying-bits.org/ )
+* @copyright (c) nickvergessen ( http://www.flying-bits.org/ )
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
